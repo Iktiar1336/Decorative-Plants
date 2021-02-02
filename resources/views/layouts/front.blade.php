03a1bc5a693330fe521ecc025a3a49a1716ec6fc
iktiar1336 <gibran1336>
2021-02-02 22:35:39 +0700

    asset css

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -12,15 +12,15 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/navbar-fixed/">
 
     <!-- Bootstrap core CSS -->
-    <link href="{{asset('/front/assets/dist/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/front/assets/dist/css/style.css')}}" rel="stylesheet">
-    <link href="{{asset('/front/assets/responsive/home/phone-responsive.css')}}" rel="stylesheet">
-    <link href="{{asset('/front/assets/responsive/home/tablet-responsive.css')}}" rel="stylesheet">
-    <link href="{{asset('/front/assets/responsive/home/desktop-responsive.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/front/assets/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/front/assets/dist/css/style.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/front/assets/responsive/home/phone-responsive.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/front/assets/responsive/home/tablet-responsive.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/front/assets/responsive/home/desktop-responsive.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('ecommerce/css/font-awesome.min.css') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link href="{{asset('/assets/vendors/mdi/css/materialdesignicons.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/assets/vendors/mdi/css/materialdesignicons.min.css')}}" rel="stylesheet">
     @yield('css')
 
     <style>
