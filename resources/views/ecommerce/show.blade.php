f68e1957fd70e5ba9149219601326295358bc605
iktiar1336 <gibran1336>
2021-02-03 23:24:33 +0700

    css home

--- resources/views/ecommerce/show.blade.php
+++ resources/views/ecommerce/show.blade.php
@@ -120,6 +120,31 @@
     </script>
     @endif
     <script src="https://kit.fontawesome.com/715497a4a3.js" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function () {
+        cartload();
+    });
+
+    function cartload()
+    {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            url: '/load-cart-data',
+            method: "GET",
+            success: function (response) {
+                $('.basket-item-count').html('');
+                var parsed = jQuery.parseJSON(response)
+                var value = parsed; //Single Data Viewing
+                $('.basket-item-count').append($('<sup>'+'<span class="badge badge-pill red">'+ value['totalcart']+'</span>'+'</sup>'));
+            }
+        });
+    }
+    </script>
 </body>
 
 </html>
