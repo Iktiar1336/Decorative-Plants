318e9b8557e58a1cbc0c04f95db6d1a61629640d
iktiar1336 <gibran1336>
2021-02-06 13:29:41 +0700

    homecontroller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,33 +33,5 @@ Decorative Plant | Home
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-md-4">
-        <div class="card text-center">
-            <div class="card-body">
-                <h3>Belum Dibayar</h3>
-                <hr>
-                <p>Rp {{ number_format($orders[0]->pending) }}</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card text-center">
-            <div class="card-body">
-                <h3>Dikirim</h3>
-                <hr>
-                <p>{{ $orders[0]->shipping }} Pesanan</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card text-center">
-            <div class="card-body">
-                <h3>Selesai</h3>
-                <hr>
-                <p>{{ $orders[0]->completeOrder }} Pesanan</p>
-            </div>
-        </div>
-    </div>
-</div>
+
 @endsection
